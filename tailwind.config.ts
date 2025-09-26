4bef385857e35cac2b4ad0f0418145bcb1405977
HasajacaKuna <pineboy@wp.pl>
2025-09-26 14:57:39 +0200

    asdasdadsad

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,18 @@
 import type { Config } from 'tailwindcss'
-import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    container: { center: true, padding: '1rem', screens: { '2xl': '1280px' } },
-    extend: {
-      fontFamily: {
-        // teraz "font-sans" korzysta z Nunito
-        sans: ['var(--font-nunito)', ...defaultTheme.fontFamily.sans],
-      },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: { '2xl': '1280px' },
     },
+    extend: {},
   },
   plugins: [],
 } satisfies Config
